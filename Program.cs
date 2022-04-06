e067ad92250bf698e11a51fb72123c5854f7dc20
petrubn <buruian.petru@ceiti.md>
2022-04-06 13:48:24 +0300

    project Upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Arrays
+{   
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine('\n');
+            Solution solve = new Solution();
+            int[] A = { 3, 8, 9, 7, 6 };
+            int K = 4;
+            
+            solve.solution(A,K);
+        }
+        public int[] solution(int[] A, int K)
+        {
+            Console.WriteLine("Orginal Array:");
+            for (int i = 0; i < A.Length; i++) Console.Write(A[i]);
+            
+            K = K % A.Length;
+            int j = 0;
+            
+            while (K > 0) 
+            { 
+                /*int temp = A.Last();*/
+                int temp = A[A.Length - 1];
+                for ( j = A.Length - 1; j > 0; j--)
+                {
+                    A[j] = A[j - 1];
+                }
+                A[j] = temp;
+                K--;
+            }
+            
+            Console.WriteLine("\n\nNew Array: ");
+            for (int i = 0; i < A.Length; i++) Console.Write(A[i]);
+            
+            return A;
+        }
+    }
+    
+}
